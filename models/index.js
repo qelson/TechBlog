91aab70b50dfd3db06a5064e4b56179e1418946c
Quin A <qelson@protonmail.com>
2023-03-10 16:54:47 -0500

    Tech Blog first draft

---
+++ models/index.js
@@ -0,0 +1,21 @@
+const User = require('./User');
+const Comment = require('./Comment');
+const Blog = require('./Blog');
+
+User.hasMany(Blog, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Blog.hasMany(Comment), {
+  foreignKey: 'blog_id'
+}
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+})
+
+module.exports = { User, Blog };
